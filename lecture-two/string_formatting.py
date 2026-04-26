1c96a2353857be030f9b1295dcd0e24d183d8f61
Abhishek Gupta <agupta8497g@gmail.com>
2026-04-25 20:27:56 -0700

    commit string formatting

---
+++ lecture-two/string_formatting.py
@@ -0,0 +1,9 @@
+first_name = 'Abhishek'
+print(f"Hi {first_name}")
+
+
+sentence = "Hi {} {}"
+last_name = "Gupta"
+print(sentence.format(first_name, last_name))
+
+print(f"Hi {first_name} {last_name} I hope you are learning")
